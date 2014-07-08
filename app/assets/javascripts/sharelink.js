ef0a435e48e3fab240cc7f174b9424e413324238
Ken Sin <kensin1129@gmail.com>
2014-07-08 12:02:57 -0400

    Share URL functionality

---
+++ app/assets/javascripts/sharelink.js
@@ -0,0 +1,41 @@
+var ShareQuotes = {
+
+  init: function() {
+    $('.quotes').on('click', '.share-quote', this.showUrl);
+    $('.container').on('click', this.removeShare);
+  },
+
+  showUrl: function(e) {
+    e.preventDefault();
+
+    var index = $(e.target).closest('.quote').index();
+    var quoteId = $(e.target).closest('.quote').attr('id');
+    var hostName = 'http://' + window.location.hostname;
+    var localhost = '';
+
+    if ( hostName.match(/localhost/) ) {
+      localhost = ':3000';
+    }
+
+    var url = hostName + localhost + '/quotes/' + quoteId;
+    var html = "<div class='share-link'><input type='text' value='" + url + "'/></div>";
+
+    $('.quotes').children(':eq(' + index + ')').after(html);
+
+  },
+
+  removeShare: function(e) {
+
+    if ($(e.target).attr('class') != 'share-quote' &&
+        !($(e.target).is('input'))) {
+      $('.share-link').remove();
+    }
+  }
+
+};
+
+$(document).ready(function() {
+
+  ShareQuotes.init();
+
+});
