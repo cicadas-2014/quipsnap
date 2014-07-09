7307274bc30adf64e5132f42356a4777e3219902
Ken Sin <kensin1129@gmail.com>
2014-07-09 11:10:36 -0400

    Hero image setup

--- app/assets/javascripts/guest.js
+++ app/assets/javascripts/guest.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
   // when there is a login button AKA user is not signed in
   if (document.getElementsByClassName('nav-signin').length == 1) {
-    $('.page-info').remove();
+
+    $('.page-info').html('Check out some recently created quotes');
+
     $('.quotes').addClass('home-quotes');
 
-    var hero = "<div class='hero-image'></div>";
+    var hero = "<div class='guest-image'><h1>Stay on the same page.</h1><h2>Sexy app</h2></div>";
 
-    $('.quotes').before(hero);
+    $('.page-info').before(hero);
   }
 });
